6d870afa2af549f6262f11df436023adb0754cde
r-pufky <r-pufky@users.noreply.github.com>
2016-02-28 13:26:31 -0800

    Initial commit
    Added base script for PDF conversion for ScanSnap Organizer Support.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # scansnap-pdf-converter
 PDF conversion script to enable ScanSnap Organizer processing.
+
+Requires [BeCyPDFMetaEdit](http://www.becyhome.de/becypdfmetaedit/description_eng.htm) to be installed.
+
+# How to use
+1. Set your input and output directories in the script
+2. Run script, accept BeCyPDFMetaEdit launching if needed from Windows UAC
+3. Import PDF's in output directory to ScanSnap Organizer
---
+++ scansnap-pdf-converter.cmd
@@ -0,0 +1,13 @@
+﻿:: Finds all pdfs in a given directory and changes Creator label to
+:: 'PFU ScanSnap Organizer' to enable use in ScanSnap.
+:: %%~dpnxF -- expands to d‎rive, p‎ath, base‎n‎ame and e‎x‎tension of file.
+::
+:: Set input and output directories accordingly. Import to ScanSnap Organizer after running.
+
+SET input="c:\Users\USERNAME\Documents\to-convert"
+SET output="c:\Users\USERNAME\Documents\converted"
+
+for %%F in (%input%\*.pdf) do (
+  "c:\Program Files (x86)\BeCyPDFMetaEdit\BeCyPDFMetaEdit.exe" "%%~dpnxF" -Creator "PFU ScanSnap Organizer"
+  move "%%~dpnxF" "%output%"
+)
